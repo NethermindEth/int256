3914379202862ad0c2d14e178995ccf441a86a2d
Ben Adams <thundercat@illyriad.co.uk>
2026-09-08 20:03:56 +0100

    Avoid discarded upper products in UInt256 MultiplyOverflow

--- src/Nethermind.Int256/UInt256.cs
+++ src/Nethermind.Int256/UInt256.cs
@@ -747,12 +747,101 @@ public readonly partial struct UInt256 : IEquatable<UInt256>, IComparable, IComp
     public void Multiply(in UInt256 a, out UInt256 res) => Multiply(this, a, out res);
 
     [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool MultiplyOverflow(in UInt256 x, in UInt256 y, out UInt256 res)
     {
-        Multiply256To512Bit(x, y, out res, out UInt256 high);
-        // Scalar test: a vector IsZero load here would span the four scalar limb stores
-        // the multiply just made and defeat store forwarding.
-        return (high.u0 | high.u1 | high.u2 | high.u3) != 0;
+        ulong xTop = x.u2 | x.u3;
+        ulong yTop = y.u2 | y.u3;
+        if ((xTop | yTop) == 0)
+        {
+            if ((x.u1 | y.u1) == 0)
+            {
+                ulong high = Multiply64(x.u0, y.u0, out ulong low);
+                Store4(out res, low, high, 0, 0);
+            }
+            else if (x.u1 == 0) MultiplyLimbs1x2(in y, x.u0, out res);
+            else if (y.u1 == 0) MultiplyLimbs1x2(in x, y.u0, out res);
+            else MultiplyLimbs2x2(in x, in y, out res);
+            return false;
+        }
+        if ((y.u1 | yTop) == 0) return MultiplyOverflowByUInt64(in x, y.u0, out res);
+        if ((x.u1 | xTop) == 0) return MultiplyOverflowByUInt64(in y, x.u0, out res);
+        if (xTop == 0)
+        {
+            if (y.u3 == 0)
+            {
+                return MultiplyOverflow2x3(in x, in y, out res);
+            }
+            MultiplyLimbs2x4(in x, in y, out res);
+        }
+        else if (yTop == 0)
+        {
+            if (x.u3 == 0)
+            {
+                return MultiplyOverflow2x3(in y, in x, out res);
+            }
+            MultiplyLimbs2x4(in y, in x, out res);
+        }
+        else MultiplyLimbs4x4(in x, in y, out res);
+        return true;
+    }
+
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void MultiplyLimbs1x2(in UInt256 x, ulong y, out UInt256 res)
+    {
+        ulong carry = Multiply64(y, x.u0, out ulong r0);
+        ulong high = Multiply64(y, x.u1, out ulong low);
+        ulong r1 = low + carry;
+        Store4(out res, r0, r1, high + (r1 < low ? 1UL : 0UL), 0);
+    }
+
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool MultiplyOverflowByUInt64(in UInt256 x, ulong y, out UInt256 res)
+    {
+        ulong carry = Multiply64(y, x.u0, out ulong r0);
+        ulong high = Multiply64(y, x.u1, out ulong low);
+        ulong r1 = low + carry;
+        carry = high + (r1 < low ? 1UL : 0UL);
+        high = Multiply64(y, x.u2, out low);
+        ulong r2 = low + carry;
+        carry = high + (r2 < low ? 1UL : 0UL);
+        high = Multiply64(y, x.u3, out low);
+        ulong r3 = low + carry;
+        bool overflow = high != 0 || r3 < low;
+        Store4(out res, r0, r1, r2, r3);
+        return overflow;
+    }
+
+    [SkipLocalsInit]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool MultiplyOverflow2x3(in UInt256 x, in UInt256 y, out UInt256 res)
+    {
+        ulong x0 = x.u0, x1 = x.u1;
+        ulong y0 = y.u0, y1 = y.u1, y2 = y.u2;
+        ulong h00 = Multiply64(x0, y0, out ulong r0);
+        ulong h01 = Multiply64(x0, y1, out ulong l01);
+        ulong h10 = Multiply64(x1, y0, out ulong l10);
+        ulong carry = 0;
+        ulong r1 = AddAndCountCarry(h00, l01, ref carry);
+        r1 = AddAndCountCarry(r1, l10, ref carry);
+        ulong r2 = carry;
+        carry = 0;
+        r2 = AddAndCountCarry(r2, h01, ref carry);
+        r2 = AddAndCountCarry(r2, h10, ref carry);
+        ulong h02 = Multiply64(x0, y2, out ulong l02);
+        r2 = AddAndCountCarry(r2, l02, ref carry);
+        ulong h11 = Multiply64(x1, y1, out ulong l11);
+        r2 = AddAndCountCarry(r2, l11, ref carry);
+        ulong r3 = carry;
+        carry = 0;
+        r3 = AddAndCountCarry(r3, h02, ref carry);
+        r3 = AddAndCountCarry(r3, h11, ref carry);
+        ulong h12 = Multiply64(x1, y2, out ulong l12);
+        r3 = AddAndCountCarry(r3, l12, ref carry);
+        Store4(out res, r0, r1, r2, r3);
+        return (h12 | carry) != 0;
     }
 
     public int BitLen =>
